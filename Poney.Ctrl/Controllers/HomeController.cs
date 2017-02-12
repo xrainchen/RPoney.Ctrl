0a0d37d3307f9de892c1401004c85e15e5c00268
Poney <Poney@PONEY-PC>
2017-02-12 22:09:17 +0800

    初始化

---
+++ Poney.Ctrl/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Poney.Ctrl.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(ParamModel model)
+        {
+
+            model.ReturnList = new List<ProductGame>();
+            for (var i = 0; i < 10; i++)
+            {
+                model.ReturnList.Add(new ProductGame() { ProductGameId = "" + i, ProductGameName = "ProductGameName" + i, ProductGameBrandName = "ProductGameBrandName" + i });
+            }
+            return View(model);
+        }
+    }
+
+    public class ParamModel
+    {
+        public List<ProductGame> ReturnList { get; set; }
+        public string SelectedIds { get; set; }
+    }
+    public class ProductGame
+    {
+        public string ProductGameId { get; set; }
+        public string ProductGameName { get; set; }
+        public string ProductGameBrandName { get; set; }
+    }
+}
